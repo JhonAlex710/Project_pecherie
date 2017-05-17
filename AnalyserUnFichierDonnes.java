2a060395e43674f4de2cb8313a7063a358ee4a32
AlfredFactice <levallois92@hotmail.com>
2017-05-17 18:21:06 +0200

    Add files via upload

--- AnalyserUnFichierDonnes.java
+++ AnalyserUnFichierDonnes.java
@@ -4,7 +4,11 @@ import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
+import donne.Annee;
 import donne.Calendrier;
+import donne.Jours;
+import donne.Lieu;
+import donne.Mois;
 import donne.ReleveDateHeureEau;
 /**
  * Sert à analyser un ensemble de donnée de port breton
@@ -42,10 +46,10 @@ public class AnalyserUnFichierDonnes extends PartagerAnalyserPlusieursAnalyseCSV
 	 * @param cheminEtNomFichier adresse du fichier
 	 * @return {@link ArrayList}{@link ReleveDateHeureEau} ensemble de sorti sous forme de liste
 	 * */
-	private ArrayList<ReleveDateHeureEau> conversion(String cheminEtNomFichier) {
+	private Mois conversion(String cheminEtNomFichier) {
 		String nomLieu= new File(new File(cheminEtNomFichier).getParent()).getName();
 		double hauteurDEau;
-		String[] date,heure;
+		String[] date={"0","0","0"},heure;
 		boolean test = true;
 		Calendrier temps;
 		List<String[]> reultat = readCsvFile(cheminEtNomFichier, ';');
@@ -59,7 +63,7 @@ public class AnalyserUnFichierDonnes extends PartagerAnalyserPlusieursAnalyseCSV
 				try {
 					hauteurDEau = Double.parseDouble(strings[2]);
 					temps = new Calendrier(Integer.parseInt(date[2]), 
-							Integer.parseInt(date[1]), Integer.parseInt(date[0]), 
+							Integer.parseInt(date[1]), Integer.parseInt(date[0]),
 							Integer.parseInt(heure[0]), Integer.parseInt(heure[1]), 0);
 				} catch (NullPointerException e) {
 					// TODO: handle exception
@@ -75,24 +79,69 @@ public class AnalyserUnFichierDonnes extends PartagerAnalyserPlusieursAnalyseCSV
 					hauteurDEau=Double.NaN;
 					temps= new Calendrier();
 					test = false;
+				}catch (Exception e) {
+					// TODO: handle exception
+					System.err.println(cheminEtNomFichier);
+					e.printStackTrace();
+					hauteurDEau=Double.NaN;
+					temps= new Calendrier();
+					test = false;
 				}
 				if (test==true) { ensembledeSortie.add(new ReleveDateHeureEau(hauteurDEau, nomLieu, temps));}
 			}
 		}
-		return ensembledeSortie;
-	}
-	
-	public void importerReleveDateHeureEauLieu() {
-		for (String[] strings : ensembleDeFichier) {
-			System.out.println(strings[1]+strings[0]);
-			System.out.println(conversion(strings[1]/*+strings[0]*/).size());
-			conversion(strings[1]/*+strings[0]*/).size();
+		/*Transformation de Arralist de ReleveDateHeureEau en 1 mois*/
+		Mois mois = new Mois(ensembledeSortie.get(0).getDateDuReleve().getMois());
+		for (ReleveDateHeureEau releveDateHeureEau : ensembledeSortie) {
+			if (mois.getListedeJours().size()==0) {
+				mois.setListedeJours(new Jours(releveDateHeureEau.getDateDuReleve()
+						.getJour()));
+				mois.getListedeJours(0).setListedeDonnes(releveDateHeureEau);
+			} else {
+				try {
+					Jours position = mois.getListedeJours(releveDateHeureEau.getDateDuReleve().getJour());
+					int numeroJours = mois.getListedeJours(position);
+					position.setListedeDonnes(releveDateHeureEau);
+					mois.setListedeJours(numeroJours,position);
+				} catch (IndexOutOfBoundsException e) {
+					// TODO: handle exception
+					Jours position=new Jours(releveDateHeureEau.getDateDuReleve().getJour());
+					position.setListedeDonnes(releveDateHeureEau);
+					mois.setListedeJours(position);
+				}
+			}
+			
 		}
+		return mois;
+		//return null;
 	}
-	/*public ArrayList<ReleveDateHeureEau> analyserUnFichierDonnesHauteurEau() {
-		String fileName="/home/jeanpaul/Bureau/ter/Donnees_Niveau Marin_BZH.csv";
-		List<String[]> reultat = readCsvFile(fileName, ';');
-		ArrayList<NiveauMarin> listeMarin=new ArrayList<NiveauMarin>(reultat.size());
-		return null;
+	/*private ArrayList<Annee> transformationAnne(ArrayList<Mois> resultat) {
+		// TODO Auto-generated method stub
+		ArrayList<Annee> ensembleDeSortie = new ArrayList<>();
+		for (Mois mois : resultat) {
+			if (ensembleDeSortie.size()==0) {
+				ensembleDeSortie.add(new Annee(resultat.get(0).getListedeJours(0).getListedeDonnes(0).
+						getDateDuReleve().getAnnee()));
+				ensembleDeSortie.get(0).
+			} else {
+
+			}
+		}
+		return ensembleDeSortie;
 	}*/
+	/**
+	 * Convertits les fichiers CSV en object. Ce ne sont des hauteurs d'eau, en des lieu. 
+	 * @return {@link ArrayList}{@link ReleveDateHeureEau} liste du contenu des fichiers
+	 * */
+	//private ArrayList<Lieu> importerReleveDateHeureEauLieu() {
+	public void importerReleveDateHeureEauLieu() {
+		ArrayList<Mois> resultat = new ArrayList<>();
+		for (String[] strings : ensembleDeFichier) {
+			//System.out.println(strings[1]);
+			resultat.add(conversion(strings[1]/*+strings[0]*/));
+		}
+		System.out.println(resultat.size());
+		//return resultat;
+		//ArrayList<Annee> resultatAnne= transformationAnne(resultat);
+	}
 }
